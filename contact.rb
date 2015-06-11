dc69a8c31327fdbba1138de9c154994a4b489ca0
cnmillar <cnmillar@gmail.com>
2015-06-11 03:26:55 +0000

    added validations

--- contact.rb
+++ contact.rb
@@ -5,6 +5,9 @@ require 'active_record'
 class Contact < ActiveRecord::Base
 
   has_many :phones
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true
 
   def to_s
     "#{id}: #{firstname} #{lastname}, #{email}, #{phone_string}"
